0461f549bf6c9ad4adc7d6f02b7d25b715db7a77
t-benedet <timothee.benedet@gmail.com>
2023-03-06 10:41:29 +0100

    Update var file

--- variables.tf
+++ variables.tf
@@ -1,3 +1,24 @@
+############################# - Auth informations - ###############################
+
+variable "subscription_id" {
+  description = "Sub ID"
+}
+
+#variable "client_id" {
+#  description = "Client ID"
+#}
+#
+#variable "client_secret" {
+#  description = "Client Secret"
+#}
+
+variable "tenant_id" {
+  description = "tenant ID"
+}
+###################################################################################
+
+
+
 variable "RESOURCELOCATION" {
   description = "Location of resource group"
   type        = string
