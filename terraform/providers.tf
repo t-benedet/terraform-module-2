4a0ec4d00a6b9efc6186d3a434f315433d33d65c
t-benedet <timothee.benedet@gmail.com>
2023-03-06 15:26:20 +0100

    Update providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
+ # subscription_id = "${var.subscription_id}"
  # client_id       = "${var.client_id}"
  # client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+ # tenant_id       = "${var.tenant_id}"
   # Configuration options
   features {}
 }
