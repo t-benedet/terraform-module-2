7db8585c3bfe32ee53eadf7087c66298ff6570eb
t-benedet <timothee.benedet@gmail.com>
2023-03-08 15:53:12 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,8 @@ resource "azurerm_resource_group" "rg" {
 
 module "kv" {
 
-    source   = "git@github.com:t-benedet/tf-azure-module-keyvault.git"
+    # source   = "git@github.com:t-benedet/tf-azure-module-keyvault.git"
+    source   = "https://github.com/t-benedet/tf-azure-module-keyvault.git"
 
     enabled_for_disk_encryption         = false
     key_permissions                     = var.key_vault_key_permissions
@@ -47,7 +48,9 @@ module "kv" {
 
 module "sa" {
 
-    source   = "git@github.com:t-benedet/tf-azure-module-storage-account.git"
+    #source   = "git@github.com:t-benedet/tf-azure-module-storage-account.git"
+    source   = "https://github.com/t-benedet/tf-azure-module-storage-account.git"
+
 
     name                        = lower("sa${var.project}${var.environment}")
     resource_group_name         = var.resource_group_name
@@ -78,7 +81,8 @@ resource "azurerm_storage_encryption_scope" "encryption" {
 
 module "blob" {
 
-    source   = "git@github.com:t-benedet/tf-azure-module-storage-container.git"
+    #source   = "git@github.com:t-benedet/tf-azure-module-storage-container.git"
+    source   = "https://github.com/t-benedet/tf-azure-module-storage-container.git"
 
     container_access_type       = "private"
     name                        = "${module.sa.name}-tfstate"
